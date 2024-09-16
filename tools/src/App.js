d42aa177fdb25a0b03592a8482480c6fc746ace0
zakya sharmeen <sakarim9124@gmail.com>
2024-09-16 12:50:06 +0530

    offcanvas-navbar

--- tools/src/App.js
+++ tools/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Bootstrap5 from './Components-tools/Bootstrap5';
+import Navbar0fcanvas from './Components-tools/Navbar0fcanvas';
 
 function App() {
   return (
    <>
-   <Bootstrap5/>
+   {/* <Bootstrap5/> */}
+   <Navbar0fcanvas/>
    </>
   );
 }
